5a98e1c4a70fd4a8b327c9fadeb637b280163673
bingfengyeyu <jackeylove0103@gmail.com>
2024-01-31 21:07:44 +0800

    FAQ做完了

--- Insurance_js/index.js
+++ Insurance_js/index.js
@@ -1,4 +1,8 @@
-function toggleContent(){
-    var content = document.getElementById('menuContent');
-    content.classList.toggle('active');
-}
+// function toggleContent(){
+//     var content = document.getElementById('menuContent');
+//     content.classList.toggle('active');
+// }
+// function toggleContent(){
+//     var content = document.getElementById('menuConten');
+//     content.classList.toggle('active');
+// }
