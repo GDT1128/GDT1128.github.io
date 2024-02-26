b59854147e45ebd6833579bd3d8a9bb0ec5395c6
bingfengyeyu <jackeylove0103@gmail.com>
2024-02-26 10:28:25 +0800

    聊天機器人介面 側邊欄

---
+++ Insurance_js/Insurance.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function(){
+
+    $("#collapse").on("click",function(){
+
+
+        $("#sidebar").toggleClass("active");
+        $(".material-symbols-outlined").toggleClass("fa-align-right");
+        
+    })  
+
+
+})
